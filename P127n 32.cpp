e481f9136f67e0bdfabff31ad5159a85d909401a
28veronica <veronica.biadene@gmail.com>
2016-11-09 16:47:56 +0100

    Esercizio
    Prova per vedere se riesco ad inserire gli esercizi

---
+++ P127n 32.cpp
@@ -0,0 +1,18 @@
+//esercizio p 127 n 32
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+  int lato, Area, Perimetro;
+  cout << "Inserire la misura del lato"  << endl;
+  cin >>lato;
+  Area= lato*lato;
+  Perimetro= lato*4;
+  cout << "l'Area e' di : " << Area << endl;
+  cout << "Il Perimetro e' di: " << Perimetro;
+
+   return 0;
+
+}
