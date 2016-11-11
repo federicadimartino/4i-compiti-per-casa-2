fbce9b0c21705e3e72708455ec875a617666ed30
28veronica <veronica.biadene@gmail.com>
2016-11-11 16:58:27 +0100

    es 31
    er

---
+++ pagina127n31.cpp
@@ -0,0 +1,16 @@
+//Pagina 127 es 31
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+   int n;
+   char stringa[n];
+   for (int i=0 ; i<=3; i++)
+   {
+    stringa[n];
+   }
+
+
+}
